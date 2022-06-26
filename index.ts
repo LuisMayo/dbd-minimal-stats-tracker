a74e9b67352674f298ca473f51b839c860afdaad
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2022-06-27 00:54:43 +0200

    Added stats

--- index.ts
+++ index.ts
@@ -3,7 +3,10 @@ const MATCHES = 'dbd-minimal-tracker-matches';
 class Match {
     timestamp!: number;
     kills!: number;
+    // Only applicable if survivor
     survived!: boolean;
+    // Only applicable if SWF
+    teamSize: number | undefined;
     gamemode!: Gamemode;
 }
 
@@ -18,6 +21,7 @@ function addMatch(type: Gamemode) {
     const match = new Match();
     match.timestamp = Date.now();
     match.kills = +(document.getElementById('killnumber') as HTMLInputElement).value;
+    match.teamSize = +(document.getElementById('killnumber') as HTMLInputElement).value;
     match.survived = (document.getElementById('survive') as HTMLInputElement).checked;
     match.gamemode = type;
     matches.push(match);
@@ -34,4 +38,46 @@ function addSwfMatch() {
 
 function addKillerMatch() {
     addMatch('killer');
+}
+
+
+function printStats() {
+    printTotalEscapeRate();
+    printKillRate();
+}
+
+function printTotalEscapeRate() {
+    const survMatches = getSurvMatches();
+    // general
+    const totalEscapes = matches.reduce((total, item) => 4 - item.kills + total, 0);
+    document.getElementById('escape-general')!.textContent = ((totalEscapes * 100) / (4 * matches.length)).toFixed(0) + '%';
+    // Mine
+    const myEscapes = survMatches.filter(item => item.survived).length;
+    document.getElementById('escape-you')!.textContent = (myEscapes * 100 / survMatches.length).toFixed(0) + '%';
+    // Others
+    // const othersEscapesWithMeInTheGame = matches.reduce((total, item) => 3 - item.kills + (item.survived ? 0 : -1) + total, 0);
+
+    // document.getElementById('escape-you')!.textContent = (myEscapes * 100 / survMatches.length).toFixed(0) + '%';
+}
+
+function printKillRate() {
+    const killerMatches = getKillerMatches();
+    const survMatches = getSurvMatches();
+    // general
+    const totalKills = matches.reduce((total, item) => item.kills + total, 0);
+    document.getElementById('kill-general')!.textContent = ((totalKills * 100) / (4 * matches.length)).toFixed(0) + '%';
+    // Mine
+    const myKills = killerMatches.reduce((total, item) => item.kills + total, 0);
+    document.getElementById('kill-you')!.textContent = (myKills * 100 / (4 * killerMatches.length)).toFixed(0) + '%';
+    // Mine
+    const otherKills = survMatches.reduce((total, item) => item.kills + total, 0);
+    document.getElementById('kill-others')!.textContent = (otherKills * 100 / (4 * survMatches.length)).toFixed(0) + '%';
+}
+
+function getSurvMatches() {
+    return matches.filter(match => match.gamemode !== 'killer');
+}
+
+function getKillerMatches() {
+    return matches.filter(match => match.gamemode === 'killer');
 }
